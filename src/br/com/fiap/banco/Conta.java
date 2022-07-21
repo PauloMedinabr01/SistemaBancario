1bfe5efa0597e3127911c8096c4a697dd01098fa
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-07-21 15:55:24 -0300

    SistemaBancario, package, class e constructor

---
+++ src/br/com/fiap/banco/Conta.java
@@ -0,0 +1,23 @@
+package br.com.fiap.banco;
+
+public class Conta {
+
+	int agencia;
+	int numero;
+	double saldo;
+	
+	
+	public Conta () {
+		
+	}
+
+	
+	public Conta(int agencia, int numero, double saldo) {
+		super();
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+
+	
+}
