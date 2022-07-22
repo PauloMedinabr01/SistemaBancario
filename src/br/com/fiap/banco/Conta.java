d5820d75f5d419e0d59cb04848489f31628cc0cf
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-07-22 14:13:13 -0300

    Criado metodos depositar, retirar e verificar saldo.

--- src/br/com/fiap/banco/Conta.java
+++ src/br/com/fiap/banco/Conta.java
@@ -5,13 +5,13 @@ public class Conta {
 	int agencia;
 	int numero;
 	double saldo;
-	
-	
+
+
 	public Conta () {
-		
+
 	}
 
-	
+
 	public Conta(int agencia, int numero, double saldo) {
 		super();
 		this.agencia = agencia;
@@ -19,5 +19,22 @@ public class Conta {
 		this.saldo = saldo;
 	}
 
-	
+	public void depositar (double valor) {
+
+		this.saldo += valor;
+
+	}
+
+	public void retirar (double valor) {
+
+		this.saldo -= valor;
+
+	}
+
+	public double verificarSaldo () {
+
+		return this.saldo;
+	}
+
+
 }
