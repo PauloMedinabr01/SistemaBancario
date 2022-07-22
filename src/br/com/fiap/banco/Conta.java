5b0a15b7b24b575920fed2057ecdc21f0fbd8d8f
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-07-22 16:27:11 -0300

    Criado documentação java.doc.

--- src/br/com/fiap/banco/Conta.java
+++ src/br/com/fiap/banco/Conta.java
@@ -1,5 +1,11 @@
 package br.com.fiap.banco;
 
+/**
+ * Classe que abstrai uma Conta Bancaria
+ * @author br_pa Paulo Medina
+ * Version 1.0
+ */
+
 public class Conta {
 
 	// Atributos de Classes
@@ -7,29 +13,44 @@ public class Conta {
 	int numero;
 	double saldo;
 
-
 	public Conta () {
 
 	}
 
+	/**
+	 * Realiza um deposito na conta corrente 
+	 * @param agencia O numero da agencia
+	 * @param numero O numero da conta
+	 * @param saldo O valor a ser depositado
+	 * @return O numero do protocolo de confirmação
+	 */
 
 	// Construtor de Classe, Classe conta
 	public Conta(int agencia, int numero, double saldo) {
+
 		super();
 		this.agencia = agencia;
 		this.numero = numero;
 		this.saldo = saldo;
+
 	}
 
 	/**
-	 * Depositar da classe Conta
+	 * Acrescenta valor ao Saldo da Conta
 	 * @param valor a ser depositado
 	 */
+
 	public void depositar (double valor) {
 
 		this.saldo += valor;
 
 	}
+
+	/**
+	 * Retira um valor do Saldo da Conta
+	 * @param valor que será retirado
+	 * @see depositar
+	 */
 
 	public void retirar (double valor) {
 
@@ -37,9 +58,15 @@ public class Conta {
 
 	}
 
+	/**
+	 * Verifica o Saldo da Conta
+	 * @return Valor do Saldo da Conta
+	 */
+
 	public double verificarSaldo () {
 
 		return this.saldo;
+
 	}
 
 
