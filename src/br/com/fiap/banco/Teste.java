dcf45ae9ed4e821bb488ab73c7ac27f4df707678
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-07-22 15:27:16 -0300

    Teste de deposito e retirada + msng de saldo.

--- src/br/com/fiap/banco/Teste.java
+++ src/br/com/fiap/banco/Teste.java
@@ -1,19 +1,26 @@
 package br.com.fiap.banco;
 
 public class Teste {
-	
+
 	public static void main(String[] args) {
-		
+
 		Conta cc= new Conta ();
 		cc.saldo = 50.0;
 		cc.agencia = 123;
 		cc.numero = 321;
-		
+
 		cc.depositar(1000);
-		
-		System.out.print("Deposito efetuado com sucesso. Seu saldo é de  ");
-		System.out.print(cc.verificarSaldo ());
-		
+
+		System.out.println( "Seu saldo em conta corrente é de R$ " + cc.verificarSaldo () );
+
+		Conta poupanca = new Conta (111, 222, 1000);
+		poupanca.retirar(50);
+
+		System.out.println( "Seu saldo em conta poupança é de R$ " + poupanca.verificarSaldo () );
+
+
+
+
 	}
 
 }
