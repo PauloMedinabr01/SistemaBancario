0b276ef93fecf19e28e0727d3fd862c391273676
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-07-22 14:42:27 -0300

    Criado o objeto conta com sld, ag e num.

--- src/br/com/fiap/banco/Teste.java
+++ src/br/com/fiap/banco/Teste.java
@@ -1,5 +1,19 @@
 package br.com.fiap.banco;
 
 public class Teste {
+	
+	public static void main(String[] args) {
+		
+		Conta cc= new Conta ();
+		cc.saldo = 50.0;
+		cc.agencia = 123;
+		cc.numero = 321;
+		
+		cc.depositar(1000);
+		
+		System.out.print("Deposito efetuado com sucesso. Seu saldo é de  ");
+		System.out.print(cc.verificarSaldo ());
+		
+	}
 
 }
